5d9b1163c3188dab88b40387237c9316d089bfba
bella <143981370+bellaacp@users.noreply.github.com>
2025-12-01 22:21:10 +0700

    resolve merge conflicts in main.go

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"time"
 
 	"github.com/gin-contrib/cors"
-=======
->>>>>>> 8952ca04f1bcdeee897cbb3f4dc61fd815186c8b
 	"github.com/gin-gonic/gin"
 	"github.com/nathchristine/PBKK_FP/controllers"
 	"github.com/nathchristine/PBKK_FP/initializers"
@@ -18,10 +15,8 @@ func init() {
 	initializers.ConnectToDB()
 
 	initializers.DB.AutoMigrate(&models.User{})
-<<<<<<< HEAD
 	initializers.DB.AutoMigrate(&models.Book{})
 	initializers.DB.AutoMigrate(&models.Transaction{})
-
 }
 
 func main() {
@@ -38,12 +33,6 @@ func main() {
 	}))
 
 	// Backend routes
-=======
-}
-func main() {
-	router := gin.Default()
-
->>>>>>> 8952ca04f1bcdeee897cbb3f4dc61fd815186c8b
 	router.POST("/books", controllers.BooksCreate)
 	router.PUT("/books/:id", controllers.BooksUpdate)
 	router.GET("/books", controllers.BooksIndex)
@@ -55,21 +44,14 @@ func main() {
 	router.GET("/transactions", controllers.TransactionIndex)
 	router.GET("/transactions/:id", controllers.TransactionShow)
 	router.DELETE("/transactions/:id", controllers.TransactionDelete)
-<<<<<<< HEAD
 	router.GET("/transactions/mine", controllers.TransactionMine)
 	router.PATCH("/transactions/:id", controllers.TransactionUpdate)
-=======
->>>>>>> 8952ca04f1bcdeee897cbb3f4dc61fd815186c8b
 
 	router.POST("/signup", controllers.Signup)
 	router.POST("/login", controllers.Login)
 
-<<<<<<< HEAD
-	router.GET("/users", controllers.UsersIndex)         
-	router.DELETE("/users/:id", controllers.UsersDelete) 
+	router.GET("/users", controllers.UsersIndex)
+	router.DELETE("/users/:id", controllers.UsersDelete)
 
 	router.Run(":8080")
-=======
-	router.Run(":3000")
->>>>>>> 8952ca04f1bcdeee897cbb3f4dc61fd815186c8b
 }
